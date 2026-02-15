44d70aa8fb2ea028a592fca64b82bfe639233ea0
Cavin <cavinlarry001@gmail.com>
2026-02-15 21:36:39 +0300

    feat: add product aggregate with a refactor of the value objects

--- src/main/java/io/github/devcavin/Main.java
+++ src/main/java/io/github/devcavin/Main.java
@@ -1,9 +1,10 @@
 package io.github.devcavin;
 
 import io.github.devcavin.domain.customers.Customer;
-import io.github.devcavin.domain.valueobject.CustomerId;
-import io.github.devcavin.domain.valueobject.EmailObject;
-import io.github.devcavin.domain.valueobject.NameObject;
+import io.github.devcavin.domain.products.Product;
+import io.github.devcavin.domain.valueobject.*;
+
+import java.math.BigDecimal;
 
 public class Main {
     public static void main(String[] args) {
@@ -11,12 +12,23 @@ public class Main {
 
         Customer customer = new Customer(
                 CustomerId.newId(),
-                new EmailObject("t@test.com"),
-                new NameObject("Test")
+                new Email("t@test.com"),
+                new Name("Test")
         );
 
         System.out.println("Customer id is: " + customer.getId());
         System.out.println("Customer email is: " + customer.getEmail());
         System.out.println("Customer name is " + customer.getName());
+
+        Product product = new Product(
+                ProductId.newId(),
+                new Title("Product 1"),
+                new Description("Product 1 description"),
+                new Money(new BigDecimal("10.00"))
+        );
+        System.out.println("Product id is: " + product.getId());
+        System.out.println("Product title is: " + product.getTitle());
+        System.out.println("Product description is: " + product.getDescription());
+        System.out.println("Product price is: " + product.getPrice());
     }
 }
